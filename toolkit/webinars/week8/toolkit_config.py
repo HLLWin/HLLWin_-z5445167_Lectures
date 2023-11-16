a5d6aff40a1a1a785f9e25e1f975b2f24c44bdae
HLLWin <148762831+HLLWin@users.noreply.github.com>
2023-11-16 20:36:02 +1100

    Add files via upload

---
+++ toolkit/webinars/week8/toolkit_config.py
@@ -0,0 +1,9 @@
+""" toolkit_config.py
+
+Project configuration file
+"""
+import os
+
+PRJDIR = r'C:\Users\win\PycharmProjects\toolkit\toolkit\toolkit_config.py'
+DATADIR = os.path.join(PRJDIR, 'data')
+
